89f4435b773029e48b6ccc5d8dc3ff7c824274fe
Tomo Masakura <tomo.masakura@gmail.com>
2015-01-24 21:30:44 +0900

    Fix #17 mistake add files.

---
+++ app/plugins.js
@@ -0,0 +1,94 @@
+'use strict';
+
+module.exports = {
+  default: [
+    {
+      name: 'org.apache.cordova.battery-status',
+      description: 'Cordova Battery Plugin'
+    },
+    {
+      name: 'org.apache.cordova.camera',
+      description: 'Cordova Camera Plugin'
+    },
+    {
+      name: 'org.apache.cordova.console',
+      description: 'Cordova Console Plugin'
+    },
+    {
+      name: 'org.apache.cordova.contacts',
+      description: 'Cordova Contacts Plugin'
+    },
+    {
+      name: 'org.apache.cordova.device',
+      description: 'Cordova Device Plugin'
+    },
+    {
+      name: 'org.apache.cordova.device-motion',
+      description: 'Cordova Device Motion Plugin'
+    },
+    {
+      name: 'org.apache.cordova.device-orientation',
+      description: 'Cordova Device Orientation Plugin'
+    },
+    {
+      name: 'org.apache.cordova.dialogs',
+      description: 'Cordova Notification Plugin'
+    },
+    {
+      name: 'org.apache.cordova.file',
+      description: 'Cordova File Plugin'
+    },
+    {
+      name: 'org.apache.cordova.file-system-roots',
+      description: 'org.apache.cordova.file-system-roots plugin ==========================================='
+    },
+    {
+      name: 'org.apache.cordova.file-transfer',
+      description: 'Cordova File Transfer Plugin'
+    },
+    {
+      name: 'org.apache.cordova.geolocation',
+      description: 'Cordova Geolocation Plugin'
+    },
+    {
+      name: 'org.apache.cordova.globalization',
+      description: 'Cordova Globalization Plugin'
+    },
+    {
+      name: 'org.apache.cordova.inappbrowser',
+      description: 'Cordova InAppBrowser Plugin'
+    },
+    {
+      name: 'org.apache.cordova.media',
+      description: 'Cordova Media Plugin'
+    },
+    {
+      name: 'org.apache.cordova.media-capture',
+      description: 'Cordova Media Capture Plugin'
+    },
+    {
+      name: 'org.apache.cordova.network-information',
+      description: 'Cordova Network Information Plugin'
+    },
+    {
+      name: 'org.apache.cordova.plugin.softkeyboard',
+      description: 'Controls Soft Keyboard on touch devices (so far only Android)$'
+    },
+    {
+      name: 'org.apache.cordova.speech.speechsynthesis',
+      description: 'Cordova Speech Synthesis Plugin'
+    },
+    {
+      name: 'org.apache.cordova.splashscreen',
+      description: 'Cordova Splashscreen Plugin'
+    },
+    {
+      name: 'org.apache.cordova.statusbar',
+      description: 'Cordova StatusBar Plugin'
+    },
+    {
+      name: 'org.apache.cordova.vibration',
+    description: 'Cordova Vibration Plugin'
+    }
+  ]
+};
