26eea5ce23398edbb11cc4653bfc8cf024ce6b72
Tomo Masakura <tomo.masakura@gmail.com>
2015-08-10 19:43:05 +0900

    Fix test failed

--- test/generatorInfo.spec.js
+++ test/generatorInfo.spec.js
@@ -6,14 +6,14 @@ describe('generatorInfo', function () {
   var generatorInfo;
 
   beforeEach(function () {
-    generatorInfo = require('generatorInfo');
+    generatorInfo = require('../app/generatorInfo.js');
   });
 
   describe('getGeneratorPath', function () {
     it('Get generator path.', function () {
       var result = generatorInfo.getGeneratorPath('/usr/lib/node_modules/generator-webapp/app/index.js');
 
-      expect(result).toEqual('/usr/lib/node/modules/generator-webapp');
+      expect(result).toEqual('/usr/lib/node_modules/generator-webapp');
     });
   });
 });
